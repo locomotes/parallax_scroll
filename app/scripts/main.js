94788a67163abdc22f424df595d090127bf6e3ac
Jonathan Dickerson <jongdickerson@gmail.com>
2014-11-02 18:16:15 -0500

    menu + sections added w/ fullpage js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('The Iron Yard Rocks');
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+    	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    	menu: '#menu'
+    });
+});
