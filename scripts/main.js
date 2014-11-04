aac26bef6aee1fd0511e11ffba7a935d1d2df55a
Jonathan Dickerson <jongdickerson@gmail.com>
2014-11-03 19:24:53 -0500

    side nav added on desktop

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+    	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    	menu: '#menu'
+    });
+});
+
+$("a").click(function() {
+	if ($("li").hasClass("active")) {
+		console.log("test");
+	}
+});
