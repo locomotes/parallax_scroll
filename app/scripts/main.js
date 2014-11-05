10c225ec6e780cd378595d16606a432ae7ade7d2
Jonathan Dickerson <jongdickerson@gmail.com>
2014-11-04 19:36:32 -0500

    added sections for acedamia

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
     	sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    	anchors: ['heroPage', 'aboutPage', 'educationPage', 'awardsPage', 'publicationsPage', 'researchPage', 'teachingPage', 'skillsPage'],
     	menu: '#menu',
     	navigation: true
     });
