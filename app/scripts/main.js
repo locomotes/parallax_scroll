220ee494d3c632b37afc6bd0d0bdfce815f5f310
Jonathan Dickerson <jongdickerson@gmail.com>
2014-11-15 18:01:09 -0500

    added bitters and mobile menu modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,3 +13,16 @@ $("a").click(function() {
 	}
 });
 
+$(document).ready(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
